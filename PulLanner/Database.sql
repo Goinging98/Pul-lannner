3f27d011367d1a5fb22761968456815db64c6886
kimjoohwan <yy19935@naver.com>
2023-07-26 01:18:17 +0900

    디비 커뮤니티 게시판 테이블 추가 - 주환

--- PulLanner/Database.sql
+++ PulLanner/Database.sql
@@ -256,6 +256,31 @@ CREATE TABLE BOARD (
 COMMIT;
 SELECT * FROM BOARD;
 
+-------------------------------------------------
+---------- Community Board 관련 테이블 -------------
+-------------------------------------------------
+CREATE TABLE COMMBOARD (
+	cbNo 	INT PRIMARY KEY AUTO_INCREMENT,
+    mNo 	INT,
+    TITLE	VARCHAR(1000),
+    CONTENT	VARCHAR(2000),
+    TYPE	VARCHAR(10),
+    ORIGINAL_FILENAME VARCHAR(100), 
+	RENAMED_FILENAME VARCHAR(100), 
+	READCOUNT INT DEFAULT 0, 
+    STATUS VARCHAR(1) DEFAULT 'Y' CHECK (STATUS IN('Y', 'N')),
+    CREATE_DATE DATETIME  DEFAULT CURRENT_TIMESTAMP, 
+    MODIFY_DATE DATETIME  DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT FK_COMMBOARD_WRITER FOREIGN KEY(mNo) REFERENCES MEMBER(mNo) ON DELETE SET NULL
+);
+
+INSERT INTO COMMBOARD VALUES(0, 1, '내 식물은 너무 멋져!', '내가 키운 선인장 너무 멋있는듯 정말 최고의 선인장이야! 선인장은 정말 멋져', 'PROUD', '원본파일명.txt', '변경된파일명.txt', DEFAULT, 'Y', DEFAULT, DEFAULT);
+INSERT INTO COMMBOARD VALUES(0, 1, '선인장은 이렇게 키우는게 정석', '선인장에게 물을 준다면 그것은 사치다. 강하게 키워야 한다. 물을 최대한 주지 말고 사막의 오아시스 처럼 주자', 'HONEY', '원본파일명.txt', '변경된파일명.txt', DEFAULT, 'Y', DEFAULT, DEFAULT);
+INSERT INTO COMMBOARD VALUES(0, 1, '가시가 더 길어진다..', '이번 기간에는 선인장의 가시가 더 두껍고 길어졌다. 손끝을 살짝 대봤는데 엄청 따갑다..', 'OBSERVE', '원본파일명.txt', '변경된파일명.txt', DEFAULT, 'Y', DEFAULT, DEFAULT);
+
+COMMIT;
+SELECT * FROM COMMBOARD;
+
 ------------------------------------------------------------------
 ------------------------- REPLY 관련 테이블 -------------------------
 ------------------------------------------------------------------
