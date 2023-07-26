24e490e25c0d2d50497bacc23079faf9d35aefec
kimjoohwan <yy19935@naver.com>
2023-07-26 17:58:00 +0900

    네이버 쇼핑 오류 주석처리 - 주환

--- PulLanner/src/main/java/com/multi/bbs/api/naver/NaverSearchAPI.java
+++ PulLanner/src/main/java/com/multi/bbs/api/naver/NaverSearchAPI.java
@@ -1,133 +1,133 @@
-package com.multi.mvc.api.naver;
-// 네이버 검색 API 예제 - 블로그 검색
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLEncoder;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.json.simple.parser.ParseException;
-
-import com.multi.mvc.news.model.vo.News;
-import com.multi.mvc.shop.model.vo.Product;
-
-
-public class NaverSearchAPI {
-
-	public static List<Product> getProductList(String query, int display, int start) {
-        String clientId = "sR7e9agRVrran0oy1CO1"; //애플리케이션 클라이언트 아이디
-        String clientSecret = "MadPis5VIy"; //애플리케이션 클라이언트 시크릿
+//package com.multi.mvc.api.naver;
+//// 네이버 검색 API 예제 - 블로그 검색
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStream;
+//import java.io.InputStreamReader;
+//import java.io.UnsupportedEncodingException;
+//import java.net.HttpURLConnection;
+//import java.net.MalformedURLException;
+//import java.net.URL;
+//import java.net.URLEncoder;
+//import java.util.HashMap;
+//import java.util.List;
+//import java.util.Map;
+//
+//import org.json.simple.parser.ParseException;
+//
+//import com.multi.mvc.news.model.vo.News;
+//import com.multi.mvc.shop.model.vo.Product;
+//
+//
+//public class NaverSearchAPI {
+//
+//	public static List<Product> getProductList(String query, int display, int start) {
+//        String clientId = "sR7e9agRVrran0oy1CO1"; //애플리케이션 클라이언트 아이디
+//        String clientSecret = "MadPis5VIy"; //애플리케이션 클라이언트 시크릿
+////        String apiURL = "https://openapi.naver.com/v1/search/news.json";  // 뉴스 JSON 결과
+//        String apiURL = "https://openapi.naver.com/v1/search/shop.json";  // 쇼핑 JSON 결과
+//        try {
+//        	query = URLEncoder.encode(query, "UTF-8");
+//        } catch (UnsupportedEncodingException e) {
+//            throw new RuntimeException("검색어 인코딩 실패",e);
+//        }
+//        
+//        StringBuilder url = new StringBuilder();
+//
+//        url.append(apiURL);
+//        url.append("?query="+query);
+//        url.append("&display="+display);
+//        url.append("&start="+start);
+//
+//        Map<String, String> requestHeaders = new HashMap<>();
+//        requestHeaders.put("X-Naver-Client-Id", clientId);
+//        requestHeaders.put("X-Naver-Client-Secret", clientSecret);
+//        String responseBody = get(url.toString(), requestHeaders);
+//
+//        List<Product> list = null;
+//        try {
+//			list = NaverSearchAPIParser.parseShop(responseBody);
+//		} catch (ParseException e) {
+//			e.printStackTrace();
+//		}
+//        return list;
+//    }
+//	
+//	public static List<News> getNewsList(String query, int display, int start) {
+//		String clientId = "sR7e9agRVrran0oy1CO1"; //애플리케이션 클라이언트 아이디
+//		String clientSecret = "MadPis5VIy"; //애플리케이션 클라이언트 시크릿
 //        String apiURL = "https://openapi.naver.com/v1/search/news.json";  // 뉴스 JSON 결과
-        String apiURL = "https://openapi.naver.com/v1/search/shop.json";  // 쇼핑 JSON 결과
-        try {
-        	query = URLEncoder.encode(query, "UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            throw new RuntimeException("검색어 인코딩 실패",e);
-        }
-        
-        StringBuilder url = new StringBuilder();
-
-        url.append(apiURL);
-        url.append("?query="+query);
-        url.append("&display="+display);
-        url.append("&start="+start);
-
-        Map<String, String> requestHeaders = new HashMap<>();
-        requestHeaders.put("X-Naver-Client-Id", clientId);
-        requestHeaders.put("X-Naver-Client-Secret", clientSecret);
-        String responseBody = get(url.toString(), requestHeaders);
-
-        List<Product> list = null;
-        try {
-			list = NaverSearchAPIParser.parseShop(responseBody);
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
-        return list;
-    }
-	
-	public static List<News> getNewsList(String query, int display, int start) {
-		String clientId = "sR7e9agRVrran0oy1CO1"; //애플리케이션 클라이언트 아이디
-		String clientSecret = "MadPis5VIy"; //애플리케이션 클라이언트 시크릿
-        String apiURL = "https://openapi.naver.com/v1/search/news.json";  // 뉴스 JSON 결과
-		try {
-			query = URLEncoder.encode(query, "UTF-8");
-		} catch (UnsupportedEncodingException e) {
-			throw new RuntimeException("검색어 인코딩 실패",e);
-		}
-		
-		StringBuilder url = new StringBuilder();
-		
-		url.append(apiURL);
-		url.append("?query="+query);
-		url.append("&display="+display);
-		url.append("&start="+start);
-		
-		Map<String, String> requestHeaders = new HashMap<>();
-		requestHeaders.put("X-Naver-Client-Id", clientId);
-		requestHeaders.put("X-Naver-Client-Secret", clientSecret);
-		String responseBody = get(url.toString(), requestHeaders);
-		
-		List<News> list = null;
-		try {
-			list = NaverSearchAPIParser.parseNews(responseBody);
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
-		return list;
-	}
-
-	
-    private static String get(String apiUrl, Map<String, String> requestHeaders){
-        HttpURLConnection con = connect(apiUrl);
-        try {
-            con.setRequestMethod("GET");
-            for(Map.Entry<String, String> header :requestHeaders.entrySet()) {
-                con.setRequestProperty(header.getKey(), header.getValue());
-            }
-            int responseCode = con.getResponseCode();
-            if (responseCode == HttpURLConnection.HTTP_OK) { // 정상 호출
-                return readBody(con.getInputStream());
-            } else { // 오류 발생
-                return readBody(con.getErrorStream());
-            }
-        } catch (IOException e) {
-            throw new RuntimeException("API 요청과 응답 실패", e);
-        } finally {
-            con.disconnect();
-        }
-    }
-
-
-    private static HttpURLConnection connect(String apiUrl){
-        try {
-            URL url = new URL(apiUrl);
-            return (HttpURLConnection)url.openConnection();
-        } catch (MalformedURLException e) {
-            throw new RuntimeException("API URL이 잘못되었습니다. : " + apiUrl, e);
-        } catch (IOException e) {
-            throw new RuntimeException("연결이 실패했습니다. : " + apiUrl, e);
-        }
-    }
-
-    private static String readBody(InputStream body){
-        InputStreamReader streamReader = new InputStreamReader(body);
-        try (BufferedReader lineReader = new BufferedReader(streamReader)) {
-            StringBuilder responseBody = new StringBuilder();
-            String line;
-            while ((line = lineReader.readLine()) != null) {
-                responseBody.append(line);
-            }
-            return responseBody.toString();
-        } catch (IOException e) {
-            throw new RuntimeException("API 응답을 읽는 데 실패했습니다.", e);
-        }
-    }
-}
+//		try {
+//			query = URLEncoder.encode(query, "UTF-8");
+//		} catch (UnsupportedEncodingException e) {
+//			throw new RuntimeException("검색어 인코딩 실패",e);
+//		}
+//		
+//		StringBuilder url = new StringBuilder();
+//		
+//		url.append(apiURL);
+//		url.append("?query="+query);
+//		url.append("&display="+display);
+//		url.append("&start="+start);
+//		
+//		Map<String, String> requestHeaders = new HashMap<>();
+//		requestHeaders.put("X-Naver-Client-Id", clientId);
+//		requestHeaders.put("X-Naver-Client-Secret", clientSecret);
+//		String responseBody = get(url.toString(), requestHeaders);
+//		
+//		List<News> list = null;
+//		try {
+//			list = NaverSearchAPIParser.parseNews(responseBody);
+//		} catch (ParseException e) {
+//			e.printStackTrace();
+//		}
+//		return list;
+//	}
+//
+//	
+//    private static String get(String apiUrl, Map<String, String> requestHeaders){
+//        HttpURLConnection con = connect(apiUrl);
+//        try {
+//            con.setRequestMethod("GET");
+//            for(Map.Entry<String, String> header :requestHeaders.entrySet()) {
+//                con.setRequestProperty(header.getKey(), header.getValue());
+//            }
+//            int responseCode = con.getResponseCode();
+//            if (responseCode == HttpURLConnection.HTTP_OK) { // 정상 호출
+//                return readBody(con.getInputStream());
+//            } else { // 오류 발생
+//                return readBody(con.getErrorStream());
+//            }
+//        } catch (IOException e) {
+//            throw new RuntimeException("API 요청과 응답 실패", e);
+//        } finally {
+//            con.disconnect();
+//        }
+//    }
+//
+//
+//    private static HttpURLConnection connect(String apiUrl){
+//        try {
+//            URL url = new URL(apiUrl);
+//            return (HttpURLConnection)url.openConnection();
+//        } catch (MalformedURLException e) {
+//            throw new RuntimeException("API URL이 잘못되었습니다. : " + apiUrl, e);
+//        } catch (IOException e) {
+//            throw new RuntimeException("연결이 실패했습니다. : " + apiUrl, e);
+//        }
+//    }
+//
+//    private static String readBody(InputStream body){
+//        InputStreamReader streamReader = new InputStreamReader(body);
+//        try (BufferedReader lineReader = new BufferedReader(streamReader)) {
+//            StringBuilder responseBody = new StringBuilder();
+//            String line;
+//            while ((line = lineReader.readLine()) != null) {
+//                responseBody.append(line);
+//            }
+//            return responseBody.toString();
+//        } catch (IOException e) {
+//            throw new RuntimeException("API 응답을 읽는 데 실패했습니다.", e);
+//        }
+//    }
+//}
--- PulLanner/src/main/java/com/multi/bbs/shop/controller/ShopController.java
+++ PulLanner/src/main/java/com/multi/bbs/shop/controller/ShopController.java
@@ -1,205 +1,205 @@
-package com.multi.bbs.shop.controller;
-
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import javax.servlet.http.HttpSession;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import com.multi.bbs.common.util.PageInfo;
-import com.multi.bbs.member.model.vo.Member;
-import com.multi.bbs.shop.model.service.ShopService;
-import com.multi.bbs.shop.model.vo.Cart;
-import com.multi.bbs.shop.model.vo.Product;
-import com.multi.bbs.shop.model.vo.ProductReply;
-import com.mysql.cj.x.protobuf.MysqlxCrud.Collection;
-
-@Controller
-public class ShopController {
-	
-	@Autowired
-	ShopService shopService;
-	
-	@GetMapping("/shop/main")
-	public String mainPage(Model model,
-			@RequestParam Map<String, Object> paramMap, 
-			@RequestParam(required = false) String[] kinds,
-			@RequestParam(required = false) String[] brands
-			) {
-		
-		int page = 1;
-		System.out.println(page);
-		System.out.println(paramMap);
-		System.out.println(Arrays.toString(kinds));
-		System.out.println(Arrays.toString(brands));
-		
-		if(paramMap.get("page") != null) {
-			try {
-				page = Integer.parseInt((String) paramMap.get("page"));
-			} catch (Exception e) {}
-		}
-		
-		if(kinds != null && kinds.length > 0) {
-			paramMap.put("kinds", Arrays.asList(kinds));
-		}else {
-			paramMap.remove("kinds");
-		}
-		
-		if(brands != null  && brands.length > 0 ) {
-			paramMap.put("brands", Arrays.asList(brands));
-		}else {
-			paramMap.remove("brands");
-		}
-		
-		int count = shopService.getProductCount(paramMap);
-		PageInfo pageInfo = new PageInfo(page, 5, count, 12);
-		List<Product> list = shopService.getProductList(pageInfo, paramMap);
-		
-		model.addAttribute("count", count);
-		model.addAttribute("list", list);
-		model.addAttribute("pageInfo", pageInfo);
-		model.addAttribute("paramMap", paramMap);
-		
-		return "/shop/shop-main";
-	}
-	
-	
-	public static int pageCount = 0;
-	
-	@GetMapping("/shop/product")
-	public String productView(Model model, int pno) {
-		Product product = shopService.getProductById(pno);
-		List<ProductReply> replyList = shopService.getProductReplyList(pno);
-		
-		
-		// 대표상품 4개, - 키보드, 마우스 2세트
-		Map<String, Object> map = new HashMap<>();
-		map.put("title", "키보드");
-		int count = shopService.getProductCount(map);
-		System.out.println(count);
-		PageInfo pageInfo = new PageInfo(pageCount++ % 5 + 1, 6, count, 6);
-		List<Product> plist1 = shopService.getProductList(pageInfo, map);
-		
-		map.put("title", "마우스");
-		count = shopService.getProductCount(map);
-		System.out.println(count);
-		pageInfo = new PageInfo(pageCount++ % 5 + 1, 6, count, 6);
-		List<Product> plist2 = shopService.getProductList(pageInfo, map);
-		plist1.addAll(plist2);
-		Collections.shuffle(plist1);
-		model.addAttribute("plist1", plist1);
-		
-		
-		model.addAttribute("product", product);
-		model.addAttribute("plist1", plist1);
-		model.addAttribute("replyList", replyList);
-		return "/shop/shop-single";
-	}
-	
-	@PostMapping("/shop/writeReply")
-	public String writeReply(Model model, ProductReply reply, HttpSession session) {
-		Member member = (Member) session.getAttribute("loginMember");
-		if(member == null) {
-			model.addAttribute("msg", "로그인이 필요합니다.");
-			model.addAttribute("location", "/shop/product?pno=" + reply.getPno());
-			return "common/msg";
-		}
-		
-		reply.setMno(member.getMno());
-		System.out.println(reply);
-		int result = shopService.insertProductReply(reply);
-		shopService.updateProductAVG(reply.getPno());
-		
-		if(result > 0) {
-			model.addAttribute("msg", "댓글이 성공적으로 작성되었습니다.");
-			model.addAttribute("location", "/shop/product?pno=" + reply.getPno());
-		}else {
-			model.addAttribute("msg", "댓글 작성에 실패하였습니다.");
-			model.addAttribute("location", "/shop/product?pno=" + reply.getPno());
-		}
-		return "common/msg";
-	}
-	
-	@PostMapping("/shop/deleteReply")
-	public String writeReply(Model model, int rno, int pno, HttpSession session) {
-		Member member = (Member) session.getAttribute("loginMember");
-		if(member == null) {
-			model.addAttribute("msg", "로그인이 필요합니다.");
-			model.addAttribute("location", "/shop/product?pno=" + pno);
-			return "common/msg";
-		}
-		int result = shopService.deleteProductReply(rno);
-		if(result > 0) {
-			model.addAttribute("msg", "댓글이 삭제 되었습니다.");
-			model.addAttribute("location", "/shop/product?pno=" + pno);
-		}else {
-			model.addAttribute("msg", "댓글 삭제에 실패하였습니다.");
-			model.addAttribute("location", "/shop/product?pno=" + pno);
-		}
-		return "common/msg";
-	}
-	
-	@RequestMapping("/shop/cart")
-	public String cartView(Model model, HttpSession session) {
-		Member member = (Member) session.getAttribute("loginMember");
-		if(member == null) {
-			model.addAttribute("msg", "로그인이 필요합니다.");
-			model.addAttribute("location", "/");
-			return "common/msg";
-		}
-		
-		List<Product> list = shopService.getCartProductList(member.getMno());
-		model.addAttribute("list", list);
-		return "shop/shop-cart";
-	}
-	
-	
-
-	// 장바구니 기능은 AJAX로 구현할것!! 아래 임시 코드나 급할때만 사용하세요.
-	@RequestMapping("/shop/addCart")
-	String addCart(Model model, HttpSession session, int pno) {
-		Member member = (Member) session.getAttribute("loginMember");
-		if(member == null) {
-			model.addAttribute("msg", "로그인이 필요합니다.");
-			model.addAttribute("location", "/");
-			return "common/msg";
-		}
-		
-		Map<String, String> map = new HashMap<>();
-		map.put("pno", ""+pno);
-		map.put("mno", ""+member.getMno());
-		
-		int result = 0;
-		List<Cart> list = shopService.getCart(map);
-		if(list == null || list.size() == 0) {
-			Cart cart = new Cart(member.getMno(), pno, 1);
-			result = shopService.insertCart(cart);
-		}else {
-			Cart cart = list.get(0);
-			cart.setAmount(cart.getAmount() + 1);
-			result = shopService.updateCart(cart);
-		}
-		
-		if(result >0) {
-			model.addAttribute("msg", "장바구니에 담겼습니다.");
-			model.addAttribute("location", "/shop/main");
-		} else {
-			model.addAttribute("msg", "장바구니 담기에 실패하였습니다.");
-			model.addAttribute("location", "/shop/main");
-		}
-		return "common/msg";
-	}
-	
-	
-	
-}
+//package com.multi.bbs.shop.controller;
+//
+//import java.util.Arrays;
+//import java.util.Collections;
+//import java.util.HashMap;
+//import java.util.List;
+//import java.util.Map;
+//
+//import javax.servlet.http.HttpSession;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//
+//import com.multi.bbs.common.util.PageInfo;
+//import com.multi.bbs.member.model.vo.Member;
+//import com.multi.bbs.shop.model.service.ShopService;
+//import com.multi.bbs.shop.model.vo.Cart;
+//import com.multi.bbs.shop.model.vo.Product;
+//import com.multi.bbs.shop.model.vo.ProductReply;
+//import com.mysql.cj.x.protobuf.MysqlxCrud.Collection;
+//
+//@Controller
+//public class ShopController {
+//	
+//	@Autowired
+//	ShopService shopService;
+//	
+//	@GetMapping("/shop/main")
+//	public String mainPage(Model model,
+//			@RequestParam Map<String, Object> paramMap, 
+//			@RequestParam(required = false) String[] kinds,
+//			@RequestParam(required = false) String[] brands
+//			) {
+//		
+//		int page = 1;
+//		System.out.println(page);
+//		System.out.println(paramMap);
+//		System.out.println(Arrays.toString(kinds));
+//		System.out.println(Arrays.toString(brands));
+//		
+//		if(paramMap.get("page") != null) {
+//			try {
+//				page = Integer.parseInt((String) paramMap.get("page"));
+//			} catch (Exception e) {}
+//		}
+//		
+//		if(kinds != null && kinds.length > 0) {
+//			paramMap.put("kinds", Arrays.asList(kinds));
+//		}else {
+//			paramMap.remove("kinds");
+//		}
+//		
+//		if(brands != null  && brands.length > 0 ) {
+//			paramMap.put("brands", Arrays.asList(brands));
+//		}else {
+//			paramMap.remove("brands");
+//		}
+//		
+//		int count = shopService.getProductCount(paramMap);
+//		PageInfo pageInfo = new PageInfo(page, 5, count, 12);
+//		List<Product> list = shopService.getProductList(pageInfo, paramMap);
+//		
+//		model.addAttribute("count", count);
+//		model.addAttribute("list", list);
+//		model.addAttribute("pageInfo", pageInfo);
+//		model.addAttribute("paramMap", paramMap);
+//		
+//		return "/shop/shop-main";
+//	}
+//	
+//	
+//	public static int pageCount = 0;
+//	
+//	@GetMapping("/shop/product")
+//	public String productView(Model model, int pno) {
+//		Product product = shopService.getProductById(pno);
+//		List<ProductReply> replyList = shopService.getProductReplyList(pno);
+//		
+//		
+//		// 대표상품 4개, - 키보드, 마우스 2세트
+//		Map<String, Object> map = new HashMap<>();
+//		map.put("title", "키보드");
+//		int count = shopService.getProductCount(map);
+//		System.out.println(count);
+//		PageInfo pageInfo = new PageInfo(pageCount++ % 5 + 1, 6, count, 6);
+//		List<Product> plist1 = shopService.getProductList(pageInfo, map);
+//		
+//		map.put("title", "마우스");
+//		count = shopService.getProductCount(map);
+//		System.out.println(count);
+//		pageInfo = new PageInfo(pageCount++ % 5 + 1, 6, count, 6);
+//		List<Product> plist2 = shopService.getProductList(pageInfo, map);
+//		plist1.addAll(plist2);
+//		Collections.shuffle(plist1);
+//		model.addAttribute("plist1", plist1);
+//		
+//		
+//		model.addAttribute("product", product);
+//		model.addAttribute("plist1", plist1);
+//		model.addAttribute("replyList", replyList);
+//		return "/shop/shop-single";
+//	}
+//	
+//	@PostMapping("/shop/writeReply")
+//	public String writeReply(Model model, ProductReply reply, HttpSession session) {
+//		Member member = (Member) session.getAttribute("loginMember");
+//		if(member == null) {
+//			model.addAttribute("msg", "로그인이 필요합니다.");
+//			model.addAttribute("location", "/shop/product?pno=" + reply.getPno());
+//			return "common/msg";
+//		}
+//		
+//		reply.setMno(member.getMno());
+//		System.out.println(reply);
+//		int result = shopService.insertProductReply(reply);
+//		shopService.updateProductAVG(reply.getPno());
+//		
+//		if(result > 0) {
+//			model.addAttribute("msg", "댓글이 성공적으로 작성되었습니다.");
+//			model.addAttribute("location", "/shop/product?pno=" + reply.getPno());
+//		}else {
+//			model.addAttribute("msg", "댓글 작성에 실패하였습니다.");
+//			model.addAttribute("location", "/shop/product?pno=" + reply.getPno());
+//		}
+//		return "common/msg";
+//	}
+//	
+//	@PostMapping("/shop/deleteReply")
+//	public String writeReply(Model model, int rno, int pno, HttpSession session) {
+//		Member member = (Member) session.getAttribute("loginMember");
+//		if(member == null) {
+//			model.addAttribute("msg", "로그인이 필요합니다.");
+//			model.addAttribute("location", "/shop/product?pno=" + pno);
+//			return "common/msg";
+//		}
+//		int result = shopService.deleteProductReply(rno);
+//		if(result > 0) {
+//			model.addAttribute("msg", "댓글이 삭제 되었습니다.");
+//			model.addAttribute("location", "/shop/product?pno=" + pno);
+//		}else {
+//			model.addAttribute("msg", "댓글 삭제에 실패하였습니다.");
+//			model.addAttribute("location", "/shop/product?pno=" + pno);
+//		}
+//		return "common/msg";
+//	}
+//	
+//	@RequestMapping("/shop/cart")
+//	public String cartView(Model model, HttpSession session) {
+//		Member member = (Member) session.getAttribute("loginMember");
+//		if(member == null) {
+//			model.addAttribute("msg", "로그인이 필요합니다.");
+//			model.addAttribute("location", "/");
+//			return "common/msg";
+//		}
+//		
+//		List<Product> list = shopService.getCartProductList(member.getMno());
+//		model.addAttribute("list", list);
+//		return "shop/shop-cart";
+//	}
+//	
+//	
+//
+//	// 장바구니 기능은 AJAX로 구현할것!! 아래 임시 코드나 급할때만 사용하세요.
+//	@RequestMapping("/shop/addCart")
+//	String addCart(Model model, HttpSession session, int pno) {
+//		Member member = (Member) session.getAttribute("loginMember");
+//		if(member == null) {
+//			model.addAttribute("msg", "로그인이 필요합니다.");
+//			model.addAttribute("location", "/");
+//			return "common/msg";
+//		}
+//		
+//		Map<String, String> map = new HashMap<>();
+//		map.put("pno", ""+pno);
+//		map.put("mno", ""+member.getMno());
+//		
+//		int result = 0;
+//		List<Cart> list = shopService.getCart(map);
+//		if(list == null || list.size() == 0) {
+//			Cart cart = new Cart(member.getMno(), pno, 1);
+//			result = shopService.insertCart(cart);
+//		}else {
+//			Cart cart = list.get(0);
+//			cart.setAmount(cart.getAmount() + 1);
+//			result = shopService.updateCart(cart);
+//		}
+//		
+//		if(result >0) {
+//			model.addAttribute("msg", "장바구니에 담겼습니다.");
+//			model.addAttribute("location", "/shop/main");
+//		} else {
+//			model.addAttribute("msg", "장바구니 담기에 실패하였습니다.");
+//			model.addAttribute("location", "/shop/main");
+//		}
+//		return "common/msg";
+//	}
+//	
+//	
+//	
+//}
