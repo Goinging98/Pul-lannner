c8c0ff51dfb9ae17bd95de2805b2d0b9f55b7118
USER <USER@DESKTOP-4VL361U>
2023-07-28 08:54:19 +0900

    build

---
+++ PulLanner/src/main/java/com/multi/bbs/Controller/PlantManageController.java
@@ -0,0 +1,34 @@
+package com.multi.bbs.Controller;
+
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+public class PlantManageController{
+	
+//	@Autowired
+//	private BoardService service;
+	
+//	@Autowired
+//	private MemberService memberService;
+	
+	private static final Logger logger = LoggerFactory.getLogger(PlantShopController.class);
+	
+	@RequestMapping(value = "/PlantShop", method = RequestMethod.GET)
+	public String home(Locale locale, Model model, HttpSession session) {
+		logger.info("Welcome plantshop! The client locale is {}.", locale);
+//		Member loginMember = memberService.login("admin", "1212");
+//		session.setAttribute("loginMember", loginMember);
+//		test();
+		return "5.1_myplant-main";
+	}
+}
+	
